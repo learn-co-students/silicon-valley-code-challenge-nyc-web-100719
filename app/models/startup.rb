2c5876b25b0726e1f32c3e517d552d15b75cb521
Michelle Wong <michellekwong2@gmail.com>
2019-10-14 15:23:12 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,89 @@
 class Startup
+    
+    attr_reader :founder
+    attr_accessor :startup, :domain
+    @@all = [ ]
 
+    def initialize(startup, founder, domain)
+        @startup = startup
+        @founder = founder
+        @domain = domain 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pivot(domain,startup_name)
+    #given a string of a domain and a string 
+    #of a name, change the domain and name of the startup
+    self.domain = domain
+    self.startup = startup_name
+    binding.pry
+    end
+
+    
+    def self.find_by_founder(founder)
+        #given a string of a founder's name, returns 
+        #the first startup whose founder's name matches
+       FundingRound.all.find do |funding|
+                funding.startup.founder == founder
+            end
+    end
+
+    def self.domains
+        # return an array of all of the different startup domains
+        self.all.collect do |startup| 
+                startup.domain
+            end
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+        #given a venture capitalist object, type of investment (as a string), and the amount invested (as a float), 
+        #creates a new funding round and associates it with that startup and venture capitalist.
+        FundingRound.new(self, venture_capitalist, type, investment)
+    end
+
+    def num_funding_rounds
+        #Returns the total number of funding rounds that the startup has gotten
+            #go through each startup, make startup == self, and count each funding round that includes startup
+        funding_rounds= FundingRound.all.select do |funding|
+                funding.startup == self
+            end
+        funding_rounds.count
+    end
+    
+    def total_funds
+        #Returns the total sum of investments that the startup has gotten
+            #go through each startup, make startup == self, and get each investment in array, add array
+        funding_rounds= FundingRound.all.select do |funding|
+            funding.startup == self
+        end
+        funding_investments= funding_rounds.collect do |funding|
+                funding.investment
+        end
+        funding_investments.inject(0, :+)
+    end
+
+    def investors
+        #Returns a unique array of all the venture capitalists that have invested in this company
+            #find out who invested in THIS company
+            funding_rounds= FundingRound.all.select do |funding|
+                funding.startup == self
+            end
+            venture_capitalists_funding= funding_rounds.collect do |funding|
+                    funding.venture_capitalist      
+            end
+            venture_capitalists_funding.uniq
+    end
+
+    def big_investors
+        #Returns a unique array of all the venture capitalists that have invested in this company 
+        #and are in the Trés Commas club
+        self.investors.select do |investor|
+            VentureCapitalist.tres_commas_club.include?(investor)
+        end
+    
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+s1 = Startup.new("Cookie House", "Dough Boy", "CookieHouse.com")
+s2 = Startup.new("Tech Museum", "George Washington", "Techy.com")
+
+v1= VentureCapitalist.new("Michelle")
+v2 = VentureCapitalist.new("Lauren")
+v3 = VentureCapitalist.new("Susie")
+v4 = VentureCapitalist.new("Henry")
+
+f1= FundingRound.new(s1, v1, "Angel", 50)
+f2 = FundingRound.new(s2, v3, "Pre-Seed", 3000000000)
+f3 = FundingRound.new(s2, v3, "Pre-Seed", 500)
+f4 = FundingRound.new(s2, v4, "Pre-Seed", 2000000000)
+f5 = FundingRound.new(s2, v2, "Pre-Seed", 4000000000)
+f6 = FundingRound.new(s1, v4, "Pre-Seed", 6000000000)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
